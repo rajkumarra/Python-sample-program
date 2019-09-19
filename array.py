0879c255041a9ba0387035aaa6a415b48d6114c4
Rajkumar Ramesh <rajkumarramesh251998@gmail.com>
2019-09-19 14:44:42 +0530

    Add array program

---
+++ array.py
@@ -0,0 +1,12 @@
+# Print the string if the length greater than 3
+students = [ 
+	'Rajkumar',
+	'Raj',
+	'Mohan',
+	'Mohanraj',
+	'Soundar'
+]
+
+for student in students:
+	if len(student) > 3
+		print(student)
