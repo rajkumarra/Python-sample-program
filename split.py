aab5ecf95b55a2d3bcc73c741449c33eba8131ab
Rajkumar Ramesh <rajkumarramesh251998@gmail.com>
2019-09-19 15:19:24 +0530

    split program

---
+++ split.py
@@ -0,0 +1,3 @@
+sentence=("Python is a general purpose programming language. Hence, you can use the programming language for developing both desktop and web applications. Also, you can use Python")
+a=sentence.split(" ")
+print(a)
