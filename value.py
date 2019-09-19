352a6c5ef70ed2473eff2f35da8fb0e18de22df1
Rajkumar Ramesh <rajkumarramesh251998@gmail.com>
2019-09-19 15:57:37 +0530

    greater smaller program

---
+++ value.py
@@ -0,0 +1,11 @@
+first_number = int(input("enter the number"))
+second_number =int(input("enter the number"))
+
+if first_number > second_number:
+	print("Firt number is greater")
+elif second_number > first_number:
+	print("Second number of greater")
+elif first_number == second_number:
+	print("Both are equal")
+
+
